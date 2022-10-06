f306bcdb2388ced2dc848a4ed8595ecf060f2f66
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-10-06 14:58:41 +0100

    Create Week1CodeWars.hs

---
+++ week-1/codewars/Week1CodeWars.hs
@@ -0,0 +1,15 @@
+import Distribution.Simple.Program.HcPkg (list)
+--greet is defined as a function that takes no arguements and returns a CONSTANT string called "Hello World".
+greet :: String
+greet = "hello world!"
+
+--Write a function that takes an array of numbers and returns the sum of the numbers.
+--The numbers can be negative or non-integer. 
+--If the array does not contain any numbers then you should return 0.
+sumCustom :: Num a => [a] -> a 
+sumCustom list = Prelude.sum (list)
+
+
+--Simple, remove the spaces from the string, then return the resultant string.
+noSpace :: String  -> String
+noSpace = concat.words
