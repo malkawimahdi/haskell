48b06dc4623d4d79ffe1865a74ec8a20ec4084b9
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-10-13 14:06:49 +0100

    Update Geometry.hs

--- week-2/tutorial/Geometry.hs
+++ week-2/tutorial/Geometry.hs
@@ -5,12 +5,14 @@ data Direction
     = North | East | South | West
     deriving (Show)
 
+-- turnLeft, takes a single Direction as a parameter and returns the corresponding direction, immediately to it's left.
 turnLeft :: Direction -> Direction
 turnLeft North = West
 turnLeft East = North
 turnLeft South = East
 turnLeft West = South
 
+--turnRight, takes a singled Direction as a parameter and returns the corresponding direction, immediately to it's right.
 turnRight :: Direction -> Direction
 turnRight North = East
 turnRight East = South
