c7981d343224a75859d4e8fed2424fc7ed9abb3c
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-10-27 09:58:45 +0100

    Update Lecture4.hs

--- week-4/lecture/Lecture4.hs
+++ week-4/lecture/Lecture4.hs
@@ -1,3 +1,21 @@
 module Week4 where
 
---Can't modify a value contained within a list. 
+-- Everything within a list have to have the same type.
+
+-- Can't modify a value contained within a list. 
+
+-- Any type with square brackets around them such as [Int] denote that it is a list of the type.
+
+-- You do not have to declare the length of the lists in Haskell. 
+
+-- Strings are lists of characters in Haskell.
+
+-- A list can contain ANYTHING that is OF THE SAME type. E.g. ["Hello","world""] :: [[Char]]. 
+                                                          --  [div, (+), (*)] :: [Int -> Int -> Int]
+                                                          -- "" :: [Char]
+                                                          -- [] :: [a]
+
+-- Any list is greater than the empty list. compare "a" "" = GT. Uses lexigraphical comparison to determine the properties of LT, EQ, GT.
+
+-- Strings are another name for [Char], so String = [Char].  
+
