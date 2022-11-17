777b0fc3807b843c85dcb41b05fceda653929984
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-11-17 15:33:17 +0000

    Update RiverCrossing.hs

--- week-7/tutorial/RiverCrossing.hs
+++ week-7/tutorial/RiverCrossing.hs
@@ -1,1 +1,15 @@
 module RiverCrossing where
+
+import Search
+
+import Data.Set (Set)
+import qualified Data.Set as Set 
+
+type Farmer = String
+type Wolf = String
+type Goat = String
+type Cabbage = String
+
+data State = State Farmer Wolf Goat Cabbage
+    deriving (Eq, Ord, Show)
+
