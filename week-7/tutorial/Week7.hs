663e336897bb6c0f5d1d2b32cf86ee3293c34ea6
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-11-17 14:50:00 +0000

    Nearly finished with the tutorial this week.

--- week-7/tutorial/Week7.hs
+++ week-7/tutorial/Week7.hs
@@ -4,14 +4,12 @@ module Week7 where
 
 import Geometry
 import Maze
+import Search 
 
 import Data.Set (Set)
 import qualified Data.Set as Set 
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Search (bfs)
-
-
 
 -- unique takes a list as input, with the constraint that the list is ordered and outputs a list with no duplicates.  
 -- unique converts the list into a set, then converts the set back to a list. 
@@ -27,6 +25,11 @@ frequencyMap list = Map.fromListWith (+) [(character, 1) | character <- list]
 furtherest :: Maze -> Set Point
 furtherest  (points, start, end) = last $ bfs (moves points) start
 
--- nearer takes a Maze as input and outputs the set of poitns that are nearer to the start point than the goal is. 
+-- nearer takes a Maze as input and outputs the set of points that are nearer to the start point than the goal is. 
 nearer :: Maze -> Set Point
-nearer (points, start, end) = Set.unions $ init $ bfs (moves points) start
+nearer (points, start, end) = Set.unions $ takeWhile (not .  Set.member end) $ bfs (moves points) start
+-- nearer (points, start, end) = Set.unions $ init $ bfs (moves points) start
+
+-- further takes a Maze as input and outputs the set of points that are further then the goal is. 
+further :: Maze -> Set Point
+further (points, start, end) = Set.unions $ drop 1 $ dropWhile (not .  Set.member end) $ bfs (moves points) start
