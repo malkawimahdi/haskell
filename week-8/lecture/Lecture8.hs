68c3d9c71da9eb375ccf56c35d2e669df25d5c42
Mahdi Malkawi <106409817+malkawimahdi@users.noreply.github.com>
2022-11-28 21:35:33 +0000

    Update Lecture8.hs

--- week-8/lecture/Lecture8.hs
+++ week-8/lecture/Lecture8.hs
@@ -1,1 +1,8 @@
 module Lecture8 where
+
+-- lastListElement taks a list and outputs a list containing the last element.
+lastListElement :: [a] -> [a]
+lastListElement [] = []
+lastListElement  (x:xs)
+    | length xs == 1 = xs
+    | otherwise = lastListElement xs
